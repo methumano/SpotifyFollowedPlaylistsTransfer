2d59e8c1929cd4ca3f42ba2e02a7db47c042a0d4
Methu <methusan.manoharan@bp.com>
2020-03-14 23:29:06 +0000

    Creation of secrets_manager.py

---
+++ secrets_manager.py
@@ -0,0 +1,12 @@
+class secrets_manager:
+    @property
+    def client_id(self):
+        return '<REPLACE_WITH_CLIENT_ID>'
+
+    @property
+    def client_secret(self):
+        return '<REPLACE_WITH_CLIENT_SECRET>'
+
+    @property
+    def redirect_uri(self):
+        return '<REPLACE_WITH_REDIRECT_URI'

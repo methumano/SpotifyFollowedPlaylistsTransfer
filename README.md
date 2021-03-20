871c92c033eaa1577558e04d8821734dae0d62b5
methumano <methumano@gmail.com>
2021-03-20 19:18:36 +0000

    Removed dependency on Spotipy module

--- README.md
+++ README.md
@@ -6,10 +6,6 @@ That is where this script comes to play! This script will gather all followed pl
 ## Prerequisites:
 1. Login to [Spotify Developer Dashboard](https://developer.spotify.com/dashboard/login) and create a new application. You should then be able to view the Client ID and Client Secret in the top left corner of the webpage. In order to create a new Redirect URI, click edit settings in the top right corner in the dashboard and add a new Redirect URI *`e.g. http://localhost:0000/callback`*. Add these to values to the respective functions in the `secrets_manager.py` file.
 
-2. [Spotipy](https://spotipy.readthedocs.io/en/2.9.0/) : Spotipy is a lightweight Python library for the Spotify Web API. With Spotipy you get full access to all of the music data provided by the Spotify platform.
-    - For Python 2.7 users: `python pip install spotipy`
-    - For Python 3.6 usersL `python3 -m pip install spotipy --user spotipy`
-
 ## Steps:
 1. Update necessary parameters in `secrets_manager.py` and then run the script.
 2. Login to the **source** Spotify account i.e. the account which holds the playlists you want to transfer over.
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-import spotipy
-import spotipy.util as util
+import urllib.parse
+import base64
+import requests
+import json
+
 from secrets_manager import secrets_manager
 
 
@@ -13,57 +15,64 @@ def introduction():
     input("Press Enter to continue...")
 
 
-def get_client(scope, username):
-    """Creates Spotify client"""
+def get_token(scope):
+    """Obtain access token"""
     secrets = secrets_manager()
-    try:
-        token = util.prompt_for_user_token(username=username,
-                                           scope=scope,
-                                           client_id=secrets.client_id,
-                                           client_secret=secrets.client_secret,
-                                           redirect_uri=secrets.redirect_uri)
-        client = spotipy.Spotify(auth=token)
-        return client
-    except:
-        sys.exit(
-            '\nCould not create client. Please ensure details entered are correct.\n')
+    encoded_redirect_uri = urllib.parse.quote(secrets.redirect_uri, safe='')
+
+    user_response = input(
+        '\nCopy and paste the following URL into your browser, and allow the web app the necessary permissions...'
+        f'\nhttps://accounts.spotify.com/authorize?client_id={secrets.client_id}&response_type=code&redirect_uri={encoded_redirect_uri}&scope={scope}\n'
+        '\nYou should then be redirected to another webpage. Please copy the URL and paste below...\n')
+
+    authorisation_code = user_response.split('code=')[1]
+    b64_encoded_credentials = base64.urlsafe_b64encode(f'{secrets.client_id}:{secrets.client_secret}'.encode()).decode()
+    headers = {'Authorization': f'Basic {b64_encoded_credentials}'}
+    data = {'grant_type': 'authorization_code', 'code': authorisation_code, 'redirect_uri': secrets.redirect_uri}
+    response = requests.post('https://accounts.spotify.com/api/token', headers=headers, data=data)
+    return json.loads(response.text).get('access_token')
 
 
 def get_playlists():
     """Gathers list of all playlist ids and respective owner ids from source account"""
     id_list = []
     scope_list = ['playlist-read-private', 'playlist-read-collaborative']
-    username = input('\nPlease enter the username ID of the source account\n')
     input('\nPlease open a browser window and login to the source Spotify account.\n'
           'Press enter to continue...\n')
     for scope in scope_list:
-        client = get_client(scope, username)
-        results = client.current_user_playlists()
-        x = results['items']
-        for item in x:
-            a = list(item['external_urls'].values())
-            playlist_id = [element.split("/playlist/", 1)[1] for element in a]
-            b = list(item['owner']['external_urls'].values())
-            owner_id = [element.split("/user/", 1)[1] for element in b]
-            playlist_name = [item['name']]
-            combined = playlist_id + owner_id + playlist_name
-            id_list.append(combined)
+        token = get_token(scope)
+        headers = {'Accept': 'application/json', 'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'}
+        for offset in range(0, 1000, 50):
+            results = requests.get(f'https://api.spotify.com/v1/me/playlists?limit=50&offset={offset}', headers=headers)
+            print(results)
+            x = json.loads(results.text).get('items')
+            for item in x:
+                a = list(item['external_urls'].values())
+                playlist_id = [element.split("/playlist/", 1)[1] for element in a]
+                b = list(item['owner']['external_urls'].values())
+                owner_id = [element.split("/user/", 1)[1] for element in b]
+                playlist_name = [item['name']]
+                combined = playlist_id + owner_id + playlist_name
+                id_list.append(combined)
+            if len(x) != 50:
+                print(f'Offset limit: {offset}')
+                break
+    print(id_list)
     return id_list
 
 
 def add_playlists(id_list):
     """Adds playlists from list into target account"""
     scope = 'playlist-modify-public'
-    username = input('\nPlease enter the username ID of the target account\n')
-    input('\nNow log out of the source Spotify account and login to the target Spotify account.\n'
-          'Press enter to continue...')
-    client = get_client(scope, username)
+    input('\nPlease open a browser window and login to the target Spotify account.\n'
+          'Press enter to continue...\n')
+    token = get_token(scope)
     count = 0
     for item in id_list:
         playlist_id = item[0]
-        owner_id = item[1]
-        client.user_playlist_follow_playlist(
-            playlist_id=playlist_id, playlist_owner_id=owner_id)
+        headers = {'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'}
+        data = {'public': 'false'}
+        requests.put(f'https://api.spotify.com/v1/playlists/{playlist_id}/followers', headers=headers, data=data)
         print('Adding playlist: ', item[2])
         count += 1
     print('\nA total of', str(count),

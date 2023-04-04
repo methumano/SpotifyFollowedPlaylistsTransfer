9a3ac19658de9857ce23a36aeab29fde8b8faf80
methu manoharan <methusan.manoharan@contino.io>
2023-04-04 19:35:38 +0100

    Open browser window automatically

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import urllib.parse
 import base64
 import requests
 import json
+import webbrowser
 
 from secrets_manager import secrets_manager
 
@@ -20,10 +21,14 @@ def get_token(scope):
     secrets = secrets_manager()
     encoded_redirect_uri = urllib.parse.quote(secrets.redirect_uri, safe='')
 
+    webbrowser.open(
+        f'https://accounts.spotify.com/authorize?client_id={secrets.client_id}&response_type=code&redirect_uri={encoded_redirect_uri}&scope={scope}'
+    )
+
     user_response = input(
-        '\nCopy and paste the following URL into your browser, and allow the web app the necessary permissions...'
-        f'\nhttps://accounts.spotify.com/authorize?client_id={secrets.client_id}&response_type=code&redirect_uri={encoded_redirect_uri}&scope={scope}\n'
-        '\nYou should then be redirected to another webpage. Please copy the URL and paste below...\n')
+        '\nPlease allow the web app the necessary permissions to function...'
+        f'\nOnce you have done this, you will be automatically redirected to a new webpage.'
+        '\nPlease copy the URL and paste below...\n')
 
     authorisation_code = user_response.split('code=')[1]
     b64_encoded_credentials = base64.urlsafe_b64encode(f'{secrets.client_id}:{secrets.client_secret}'.encode()).decode()
@@ -32,13 +37,12 @@ def get_token(scope):
     response = requests.post('https://accounts.spotify.com/api/token', headers=headers, data=data)
     return json.loads(response.text).get('access_token')
 
-
 def get_playlists():
     """Gathers list of all playlist ids and respective owner ids from source account"""
     id_list = []
     scope_list = ['playlist-read-private', 'playlist-read-collaborative']
     input('\nPlease open a browser window and login to the source Spotify account.\n'
-          'Press enter to continue...\n')
+          'Press Enter once you are logged in and ready to continue...\n')
     for scope in scope_list:
         token = get_token(scope)
         headers = {'Accept': 'application/json', 'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'}
@@ -65,7 +69,7 @@ def add_playlists(id_list):
     """Adds playlists from list into target account"""
     scope = 'playlist-modify-public'
     input('\nPlease open a browser window and login to the target Spotify account.\n'
-          'Press enter to continue...\n')
+          'Press Enter once you are logged in and ready to continue...\n')
     token = get_token(scope)
     count = 0
     for item in id_list:
